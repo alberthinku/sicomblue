dc214056b8d53456a26cd8eb22d3bde70e2bf2d5
alberthinku <yahui.sun@gmail.com>
2019-09-14 21:37:47 +0100

    bug removed for //success status

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const server = http.createServer((req, res) => {
                         //Success
                         // fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
                         res.writeHead(200, { "Content-Type": "text/html" });
-                        res.end(content, 'utf8');
+                        res.end("file POST request served!");
                         console.log("file POST requst served!");
                         // })
 
