637b58f5752258bb3eba98e79e5a918718756d71
alberthinku <yahui.sun@gmail.com>
2019-10-25 17:12:45 +0100

    v1.1.12 support madgwick class, sync fusion2eular

--- public/libraries/filter/fusionQuaternion2Eular.js
+++ public/libraries/filter/fusionQuaternion2Eular.js
@@ -1,16 +1,3 @@
-safe_asin = function (v) {
-    // console.log(v);//watch how the v is impacting the asin.
-    if (isNaN(v)) {
-        return 0.0;
-    }
-    if (v >= 1.0) {
-        return Math.PI / 2;
-    }
-    if (v <= -1.0) {
-        return -Math.PI / 2;
-    }
-    return Math.asin(v);
-}
 
 //Quaternion elements are {w,v} or {w, qi, qj, qk} and those elements should be normalized before makeing the 2Eular process
 function fusionQuaternion2Eular(w, qi, qj, qk) {
